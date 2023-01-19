b2a7e9cc71d1ce597735d650b3a237a0727cda7b
bharat-khairnar <bharat.khairnar@bacancy.com>
2023-01-19 14:48:03 +0530

    add basename into he browser router for prefix

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename='/crwn-clothing'>
      <PersistGate persistor={persistor}>
         <React.StrictMode>
             <App />
